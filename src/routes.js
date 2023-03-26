b8ad42542ed9fd7611c82b34f26fe31d6387c647
tranquochuy645 <tranquochuy645@gmail.com>
2023-03-26 19:36:16 +0700

    ok

--- src/routes.js
+++ src/routes.js
@@ -46,6 +46,41 @@ async function route_create_user (req, res) {
         
 
 };
+
+
+async function route_update_user_data(req, res) {
+    const mongo_connection = await new_database_connection();
+
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const user_data = req.body;
+
+    const db = mongo_connection.db('database-app');
+
+    // Check if username and password are correct
+    const user = await db.collection('users').findOne({ 'username': username });
+    if (!user || user.password !== password) {
+        res.status(401).send('Incorrect username or password');
+        close_database_connection(mongo_connection);
+        return;
+    }
+
+    // Update user data in users-database collection
+    db.collection('users-database').updateOne({ 'username': username }, { $set: user_data }, { upsert: true })
+        .then(() => {
+            console.log(`User data for "${username}" updated`);
+            res.status(200).send('User data updated successfully');
+            close_database_connection(mongo_connection);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error updating user data');
+            close_database_connection(mongo_connection);
+        });
+};
+
+
 module.exports = {
-    route_create_user
+    route_create_user,
+    route_update_user_data
 };
